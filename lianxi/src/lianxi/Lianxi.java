1ee486a735116548804cbb6259877e20f370480b
Antbug <583443598@qq.com>
2017-11-24 12:54:53 +0800

    tijiao2

---
+++ lianxi/src/lianxi/Lianxi.java
@@ -0,0 +1,17 @@
+package lianxi;
+
+public class Lianxi {
+  public static void main(String[] arga){
+	  String name ="ËæÑÇÌÎ";
+	  float height =173f;
+	  int tizhong =80;
+	  char sex ='ÄÐ';
+      double price =99.5;
+      boolean dui =true;
+      System.out.println(name);
+      System.out.println(tizhong);
+	  System.out.println(sex);
+	  System.out.println(price);
+      System.out.println(dui);
+  }
+}

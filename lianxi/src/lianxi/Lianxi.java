433c78fcb1c20b28ed553d86aad3cafc95b1fbc9
Antbug <583443598@qq.com>
2017-11-24 13:44:31 +0800

    上

--- lianxi/src/lianxi/Lianxi.java
+++ lianxi/src/lianxi/Lianxi.java
@@ -2,16 +2,7 @@ package lianxi;
 
 public class Lianxi {
   public static void main(String[] arga){
-	  String name ="随亚涛";
-	  float height =173f;
-	  int tizhong =80;
-	  char sex ='男';
-      double price =99.5;
-      boolean dui =true;
-      System.out.println(name);
-      System.out.println(tizhong);
-	  System.out.println(sex);
-	  System.out.println(price);
-      System.out.println(dui);
+	 String love ="爱";
+	 System.out.println("变量love的意思是:"+love);
   }
 }
---
+++ zidongleixingzhuanhuan/src/zidong/Zidong.java
@@ -0,0 +1,13 @@
+package zidong;
+
+public class Zidong {
+	public static void main(String[] args){
+	  double fen1 =78.5;
+	  int shu =6;
+	  double fen2 =fen1+shu;
+	  System.out.println("考试平均分"+fen1);
+	  System.out.println("调整后的平均分"+fen2);
+		
+		
+	}
+}

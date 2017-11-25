2017b60a005951d639d32bdf24a9fcb7784256af
lijingwei1209 <1004451027@qq.com>
2017-11-25 11:17:49 +0800

    fx

---
+++ java/src/lesson1/One.java
@@ -0,0 +1,8 @@
+package lesson1;
+
+public class One {
+	//练习hello world
+	public static void main(String[] args){
+	 System.out.println("你好，涛");		 
+	}
+}
---
+++ java/src/lesson2/One.java
@@ -0,0 +1,14 @@
+package lesson2;
+
+public class One {
+	//练习运算符
+	public static void main(String[] args){
+	 System.out.println(1+2);	
+	 System.out.println(1<2);		 
+	 System.out.println(true||false);
+	 int a = 2;
+	 System.out.println(a++);
+	 System.out.println(a);
+	 System.out.println(++a);
+	}
+}

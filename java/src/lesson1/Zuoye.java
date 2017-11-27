6ac7c8115ad3d65003f23b2a031578b0137094bc
Antbug <583443598@qq.com>
2017-11-27 16:58:26 +0800

    xin

---
+++ java/src/lesson1/Zuoye.java
@@ -0,0 +1,18 @@
+package lesson1;
+
+public class Zuoye {
+  public static void main(String[] args){
+	int i = 0;
+	  do{
+		   
+			i++;
+		 
+		if(i==4)continue;
+		System.out.println(i);
+	
+	}while(i<=9); 
+	  
+	
+
+  }
+  }
---
+++ java/src/lesson3/Zuoye4.java
@@ -0,0 +1,19 @@
+package lesson3;
+
+public class Zuoye4 {
+   public static void main(String[] srgs){
+	   int i=0;
+	   while(i<=9){ 
+		   i++;
+		   if(i==4)continue;
+		   
+		   System.out.println(i);
+		
+	   }
+	   
+	   }
+	   
+	   
+	   
+   }
+

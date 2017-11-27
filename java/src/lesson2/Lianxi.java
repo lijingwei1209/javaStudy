ca9d2bf0b7603676908ea4a68d6773c46888108a
Antbug <583443598@qq.com>
2017-11-28 00:40:52 +0800

    jiaol

---
+++ java/src/lesson2/Lianxi.java
@@ -0,0 +1,12 @@
+package lesson2;
+  //神     判断一个变量是奇数还是偶数我这样写对吧
+public class Lianxi {
+  public static void main(String[] args){
+	  int x =121;
+	  if(x%2==0){
+		  System.out.println("偶数");
+	  }else {
+		  System.out.println("奇数");
+	  }
+  }
+}

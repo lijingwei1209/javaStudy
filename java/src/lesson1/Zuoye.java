74ac4773bbd91414d1174e7ce634d1c3e788298e
lijingwei1209 <1004451027@qq.com>
2017-11-29 10:40:39 +0800

    fx

--- java/src/lesson1/Zuoye.java
+++ java/src/lesson1/Zuoye.java
@@ -2,17 +2,13 @@ package lesson1;
 
 public class Zuoye {
   public static void main(String[] args){
-	int i = 0;
-	  do{
-		   
-			i++;
-		 
-		if(i==4)continue;
-		System.out.println(i);
-	
-	}while(i<=9); 
+	int i = 1;
+	do{
+		if(i!=4) {
+			System.out.println(i);
+		}
+		i++;
+	}while(i<=10); 
 	  
-	
-
-  }
+  	}
   }

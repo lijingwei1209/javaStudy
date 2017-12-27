f7823fff90d62fc91f2e0f9b625e7f81b3c42a21
Antbug <583443598@qq.com>
2017-12-27 17:42:57 +0800

    hao

--- java/src/test/Shuzu1.java
+++ java/src/test/Shuzu1.java
@@ -1,5 +1,7 @@
 package test;
 
 public class Shuzu1 {
-
+	public static void main(String[] args){
+      Test c = new Test();
+      }
 }
---
+++ java/src/test/Test.java
@@ -0,0 +1,19 @@
+package test;
+
+public class Test {
+   /**public static void main(String[] args){
+	   for(int i = 1;i<=9;i++){
+		   for(int j = 1;j<=i;j++){
+			   System.out.print(i+"X"+j+"="+(i*j)+"\t");
+			  
+		   }
+		   System.out.println();
+	   }
+	   
+	   
+   }*/
+	public Test(){
+		System.out.println("制造一个汽车!");
+	}
+	
+}

1d16bee35b19af4b6d7f5b4597af73cbb82b40a9
李静伟 <1004451027@qq.com>
2018-05-28 21:37:01 +0800

    fx

--- TestServlet1/src/servlet/HelloServlet.java
+++ TestServlet1/src/servlet/HelloServlet.java
@@ -14,8 +14,10 @@ public class HelloServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("以Get方式请求访问...");
-		PrintWriter out = response.getWriter();
+		response.setCharacterEncoding("UTF-8"); 
 		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+
 		out.println("能!!!");
 	}
 
@@ -23,8 +25,9 @@ public class HelloServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		System.out.println("以Post方式请求访问...");
-		PrintWriter out = response.getWriter();
+		response.setCharacterEncoding("UTF-8"); 
 		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
 		out.println("能个狗屁又是乱码!!!");
 	}
   
